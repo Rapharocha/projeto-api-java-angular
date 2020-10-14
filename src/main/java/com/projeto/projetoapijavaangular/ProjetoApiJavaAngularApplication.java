786bf575ec83ed4cc2c44b9a2405a84f8535c15d
Raphael Rocha <raphaelrocha.gft@sulamerica.com.br>
2020-10-14 15:56:42 -0300

    iniciando api rest

---
+++ src/main/java/com/projeto/projetoapijavaangular/ProjetoApiJavaAngularApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.projetoapijavaangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiJavaAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiJavaAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/projetoapijavaangular/ProjetoApiJavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.projetoapijavaangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiJavaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
